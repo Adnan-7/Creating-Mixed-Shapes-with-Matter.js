1914a076ae7fb485bdf2bea1b6cd0a5d88cc56ec
Adnan Khan <adnan7.zain@gmail.com>
2021-06-27 11:20:55 +0500

    generating random shapes

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const width = 800;
+const height = 600;
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
+    wireframes: false,
+    width,
+    height,
   },
 });
+
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+//walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+];
+
+World.add(world, walls);
+
+for (let i = 0; i <= 35; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 90, 30)
+    );
+    if (Math.random() > 0.2) {
+      World.add(
+        world,
+        Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+      );
+    }
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35)
+    );
+  }
+}
